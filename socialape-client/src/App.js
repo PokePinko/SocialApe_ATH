31eeabd4636810a5f4e0cf47b096f681984a0127
Nayth W <Nayth.sphere@gmail.com>
2020-09-10 10:41:34 +0200

    Initial front end commit

---
+++ socialape-client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import home from "./pages/home";
+import signup from "./pages/signup";
+import login from "./pages/login";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
